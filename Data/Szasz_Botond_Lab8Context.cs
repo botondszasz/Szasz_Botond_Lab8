bc8509714e7967ac57137ae6e8472a91ef7d9d4c
Botond Szasz <szaszboti999@gmail.com>
2021-11-23 15:29:14 +0200

    Add project files.

---
+++ Data/Szasz_Botond_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Szasz_Botond_Lab8.Models;
+
+namespace Szasz_Botond_Lab8.Data
+{
+    public class Szasz_Botond_Lab8Context : DbContext
+    {
+        public Szasz_Botond_Lab8Context (DbContextOptions<Szasz_Botond_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Szasz_Botond_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Szasz_Botond_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Szasz_Botond_Lab8.Models.Category> Category { get; set; }
+    }
+}
